0051a6dbe72ee69ae68e890ef461c5774cbce7f1
Pavol Rusnak <stick@gk2.sk>
2014-02-24 01:20:37 +0100

    strip linux plugin; rename dist to browser-plugin-trezor

--- linux-packaging/prepare-tarball.sh
+++ linux-packaging/prepare-tarball.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 VERSION=0.1
-tar cfj dist/browser-plugin-trezor-$VERSION.tar.bz2 --transform 's:^:browser-plugin-trezor/:' INSTALL npBitcoinTrezorPlugin.{32,64}bit.so trezor-udev.rules
+strip npBitcoinTrezorPlugin.{32,64}bit.so
+tar cfj browser-plugin-trezor/browser-plugin-trezor-$VERSION.tar.bz2 --transform 's:^:browser-plugin-trezor/:' INSTALL npBitcoinTrezorPlugin.{32,64}bit.so trezor-udev.rules
